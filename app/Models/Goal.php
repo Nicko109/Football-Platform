5ddff465557a06669d708554c236601214291b8a
nkaragodov@inbox.ru <Nevermore109>
2023-12-05 17:57:58 +0300

    Круд игр - в ограниченном виде дата игры команда 1 и команда 2 победитель + добавление голов для всех и кнопка играть

---
+++ app/Models/Goal.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Goal extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $guarded = false;
+
+    public function player()
+    {
+        return $this->belongsTo(Player::class);
+    }
+
+    public function team()
+    {
+        return $this->belongsTo(Team::class);
+    }
+
+    public function game()
+    {
+        return $this->belongsTo(Game::class);
+    }
+}
---
+++ database/migrations/2023_12_05_061830_create_goals_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('goals', function (Blueprint $table) {
+            $table->id();
+            $table->integer('count')->nullable();
+            $table->foreignId('player_id')->nullable()->index()->constrained('players');
+            $table->foreignId('team_id')->nullable()->index()->constrained('teams');
+            $table->foreignId('game_id')->nullable()->index()->constrained('games');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('goals');
+    }
+};
